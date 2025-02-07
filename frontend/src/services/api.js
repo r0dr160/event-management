3465ca28737f931f1ac0cb0490e43720490ab957
r0dr160 <2401857@estudante.uab.pt>
2025-02-07 17:25:54 +0000

    Atualiza baseURL para o backend no Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://backend-event-management-t54t.onrender.com',
 });
 
 export default {
